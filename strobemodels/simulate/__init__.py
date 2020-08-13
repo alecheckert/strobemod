3404959a1f8381674083c044b418e6bc084e3597
Alec Heckert <alecheckert@gmail.com>
2020-08-13 07:57:10 -0700

    easier imports from strobemodels.simulate

--- strobemodels/simulate/__init__.py
+++ strobemodels/simulate/__init__.py
@@ -3,3 +3,18 @@
 simulate.__init__.py
 
 """
+from .levy import LevyFlight3D
+from .fbm import (
+    FractionalBrownianMotion,
+    FractionalBrownianMotion3D
+)
+from .simutils import (
+    sample_sphere,
+    tracks_to_dataframe
+)
+from .hilosim import (
+    strobe_infinite_plane, 
+    strobe_one_state_infinite_plane,
+    strobe_two_state_infinite_plane,
+    strobe_three_state_infinite_plane
+)
