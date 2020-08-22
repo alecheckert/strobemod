1478d126a63223afd04f0bc6625b1bfc77393b05
Alec Heckert <alecheckert@gmail.com>
2020-08-21 17:59:10 -0700

    broke some stuff that will need fixing; replaced radial projection with numerical Abel transforms, which is basically the same thing

--- setup.py
+++ setup.py
@@ -1,10 +1,26 @@
 #!/usr/bin/env python
 """
-setup.py
+setup.py -- install the strobemodels package in the current
+environment and also unzip some of the larger data files
 
 """
+import os
 import setuptools
+from zipfile import ZipFile
 
+# Unzip large data files
+REPO_DIR = os.path.split(os.path.abspath(__file__))[0]
+DATA_DIR = os.path.join(REPO_DIR, "strobemodels", "data")
+targets = [
+    os.path.join(DATA_DIR, "free_abel_transform.zip")
+]
+for t in targets:
+  print("unzipping {}...".format(t))
+  out_path = os.path.split(t)[0]
+  with ZipFile(t) as f:
+    f.extractall(out_path)
+
+# Install
 setuptools.setup(
     name="strobemodels",
     version="1.0",
