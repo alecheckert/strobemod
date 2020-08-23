c6a90ed14cacd63885dbb321d2d16f8155e536e6
Alec Heckert <alecheckert@gmail.com>
2020-08-23 12:58:40 -0700

    overhauled strobemodels.simulate.levy, adding new and faster simulation routines

--- strobemodels/simulate/levy.py
+++ strobemodels/simulate/levy.py
@@ -3,87 +3,130 @@
 levy.py -- simulate 3D Levy flights
 
 """
+# Paths
 import os
+
+# Numeric
 import numpy as np 
-from scipy.stats import levy_stable 
-from strobemodels.simulate.simutils import (
-    sample_sphere
-)
+
+# Plotting
+import matplotlib.pyplot as plt 
+
+# Sample points uniformly from the surface of a sphere
+from strobemodels.simulate.simutils import sample_sphere
+
+# Custom utilities
 from strobemodels.utils import (
     HankelTrans3D,
     spline,
     pdf_from_cf_rad,
-    radnorm 
+    radnorm
 )
 
 class LevyFlight3D(object):
     """
     Simulate a 3D Levy flight using an approximate inverse CDF approach.
 
-    description
-    -----------
+    description of stochastic process
+    ---------------------------------
         LevyFlight3D simulates a Markovian diffusion process whose 3D
         radial displacements are distributed according to a Levy stable
-        random variable, and whose angular displacements are isotropically
+        random vector, and whose angular displacements are isotropically
         distributed on the surface of the unit sphere.
+
+    description of simulation method
+    --------------------------------
+        LevyFlight3D uses one of two different methods to approximate the CDF
+        for the radial displacements of a Levy flight, which is subsequently
+        used for inverse CDF sampling. Both methods are based on the inverse
+        Fourier transform of the Levy flight's characteristic function.
+
+        The first method, "hankel", uses an explicit 3D inverse spherical Fourier
+        transform of the CF to evaluate the PDF at a discrete set of bins.
+
+        The second method, "radon", uses a convenient formula derived from the
+        inverse Radon transform of the PDF. This corresponds to a modification of
+        the CF and then an inverse 1D Fourier transform.
+
+        "radon_alt" is a slight modification of "radon" that may be more 
+        accurate for very long displacements.
+
+        In both cases, once the PDF is evaluated at a finite set of bins, it
+        is accumulated to approximate the CDF. A spline interpolation of the 
+        CDF is then used to generate the inverse CDF function for sampling.
+
+        Both methods improve in accuracy as the bin size is decreased. Ideally
+        the bin size should be small enough that the CDF is approximately linear
+        in the space between each bin, at which point the spline interpolation
+        becomes very good.
+
+        The direction of the displacements is chosen by sampling points 
+        uniformly from the surface of the unit sphere.
 
     init
     ----
-        alpha       :   float between 0.0 and 2.0, the stability parameter
-                        for the Levy flight
-        scale       :   float, the dispersion parameter. For alpha = 2.0,
-                        this is equivalent to the square root of the 
-                        diffusion coefficient.
-        dt          :   float, the interval between frames in seconds
-        interpolation_range :   (float, float, float), arguments to 
-                        np.linspace generating the support to use by default
-                        for CDF sampling. The default range (0.0, 10.0, 10001)
-                        works well up to a diffusion coefficient of about 80.0
-                        (scale = sqrt(80)) for Brownian motion.
+        R_max       :   float, the maximum radial displacement to consider in um
+        bin_size    :   float, the size of the interpolation bins in um
+        alpha       :   float between 0.0 and 2.0, Levy stability parameter
+        D           :   float, the dispersion parameter
+        dt          :   float, frame interval in seconds
+        method      :   str, either "radon", "radon_alt", or "hankel", the method
+                        to use to simulate the radial displacements of the Levy flight.
+                        These are mathematically identical ways to accomplish
+                        the same thing. "hankel" is more naive, but is also slower.
+        track_len   :   int, the number of "frames" (points) per trajectory
 
     example simulation
     ------------------
-
-        # Generate the simulator object (here, Brownian motion)
-        L = LevyFlight3D(2.0, 1.0)
+        # Generate the simulator object. Here, we use Brownian motion, considering
+        # displacements up to 40.0 um
+        L = LevyFlight3D(40.0, alpha=2.0, D=2.0, dt=0.01, method="radon")
 
         # Generate 100000 trajectories of length 10
-        tracks = L(100000, 10)
+        tracks = L(100000, track_len=10)
 
     methods
     -------
-        cf          :   evaluate the characteristic function
-        cdf_rad     :   evaluate the CDF for the 3D radial displacements
-        pdf_rad     :   evaluate the PDF for the 3D radial displacements
-        cdf_rad_dev :   evaluate the first derivative of the CDF for the 
-                        3D radial displacements
-        inverse_cdf :   evaluate the inverse CDF function for arguments
-                        between 0.0 and 1.0
+        cf              :   evaluate the characteristic function
+        pdf             :   evaluate the (approximate) probability mass function
+        cdf             :   evaluate the (approximate) cumulative distribution function
+        cdf_dev         :   evaluate the first derivative of the (approximate)
+                            cumulative distribution function
+        inverse_cdf     :   evaluate the (approximate) inverse cumulative distribution
+                            function
 
     """
-    def __init__(self, alpha, scale, dt=0.01, track_len=10, interpolation_range=(0.0, 10.0, 10001)):
-        self.alpha = alpha 
-        self.scale = scale
-        self.dt = dt 
-        self.track_len = track_len 
-        self.interpolation_range = interpolation_range 
+    def __init__(self, R_max, bin_size=0.001, alpha=2.0, D=1.0, dt=0.01,
+        method="radon", track_len=10):
 
-    def __call__(self, N, track_len=None, n_iter=10):
+        assert method in ["radon", "hankel", "radon_alt"]
+
+        self.R_max = R_max 
+        self.bin_size = bin_size 
+        self.alpha = alpha 
+        self.D = D 
+        self.dt = dt 
+        self.method = method 
+        self.track_len = track_len 
+
+        # Generate real and frequency domain supports
+        self._generate_support()
+
+    def __call__(self, N, track_len=None, n_iter=20):
         """
         Simulate *N* instances of this Levy flight.
 
         args
         ----
             N           :   int, the number of Levy flights to simulate
-            track_len   :   int, the length of each trajectory to 
-                            simulate
-            n_iter      :   int, the number of iterations of Newton's
-                            method to use for inverse CDF sampling
+            track_len   :   int, the length of each trajectory to simulate
+            n_iter      :   int, the number of iterations of Newton's method
+                            to use for inverse CDF sampling
 
         returns
-        -------
-            3D ndarray, shape (N, traj_len, 3), the ZYX positions
-                of each Levy flight at each timepoint
+        ------- 
+            3D ndarray, shape (N, track_len, 3), the ZYX positions of each
+                Levy flight at each frame
 
         """
         if track_len is None:
@@ -107,171 +150,228 @@ class LevyFlight3D(object):
         v = np.cumsum(v, axis=1)
         return v 
 
-    def cf(self, k):
+    def _generate_support(self):
         """
-        Evaluate the characteristic function for the radial displacements
-        of the Levy flight.
+        Generate the real domain and frequency domain supports for this Levy flight.
+
+        """
+        # The edges of each interpolation bin. Note that for the Radon method, 
+        # we need to evaluate twice as many points in the real domain as the user
+        # specifies, to sufficiently separate the positive and negative parts of 
+        # the Radon transform.
+        self.r_full = np.arange(0.0, 2*self.R_max+self.bin_size, self.bin_size)
+
+        # The centers of each interpolation bin 
+        self.rc_full = self.r_full[:-1] + self.bin_size * 0.5
+
+        # The last real domain point to use, for the "radon" method
+        self.cutoff = self.r_full.shape[0] // 2
+
+        # Truncated versions of the real domain supports
+        self.r = self.r_full[:self.cutoff+1]
+        self.rc = self.rc_full[:self.cutoff]
+
+        # The frequency domain 
+        self.k = 2.0 * np.pi * np.fft.rfftfreq(self.r_full.shape[0], d=self.bin_size)
+
+    def cf(self, k=None):
+        """
+        Evaluate the characteristic function for the 3D radial displacements of 
+        this Levy flight, expressed as a function of the radial frequency
+        coordinate. That is, 
+
+            k^2 = k_{x}^2 + k_{y}^2 + k_{z}^2 
+
 
         args
         ----
-            k       :   real-valued 1D ndarray, evaluation points
+            k       :   1D ndarray. If *None*, default to this object's initial
+                            support (self.k)
 
         returns
         -------
-            1D ndarray, dtype float64, the characteristic function
+            1D ndarray, dtype float64, shape k.shape; the CF
 
         """
-        return np.exp(-self.scale * self.dt * np.power(np.abs(k), self.alpha))
+        if k is None:
+            k = self.k
+        return np.exp(-self.D * self.dt * np.power(np.abs(k), self.alpha))
 
-    def cdf_rad(self, r):
+    def pdf(self, r=None, normalize=True):
         """
-        Approximate the CDF for the 3D radial displacements.
+        Evaluate the probability density function for the 3D radial 
+        displacements of this Levy flight. Note that this is really a 
+        scaled approximation to the PMF rather than the PDF, since 
+        we may evaluate it on a support with different bin size than the 
+        support originally used to generate the PDF (self.rc).
 
         args
         ----
-            r       :   1D ndarray, points at which to evaluate CDF
+            r           :   1D ndarray, the set of points in the real
+                            domain at which to evaluate the PDF
+            normalize   :   bool, normalize this PDF to sum to 1
 
         returns
         -------
-            1D ndarray, the CDF 
+            1D ndarray, dtype float64, size r.shape; the PDF
 
         """
+        # If not specified, default to the initial real domain support
+        if r is None:
+            r = self.rc[:self.cutoff]
+
+        # Generate a spline interpolation to the PDF, if not done already
+        if not hasattr(self, "_pdf_rad"):
+            self._gen_pdf_rad()
+
+        # Evaluate 
+        result = self._pdf_rad(r)
+
+        # Normalize
+        if normalize:
+            result /= result.sum()
+        return result 
+
+    def cdf(self, r=None):
+        """
+        Evaluate the cumulative distribution function for the 3D radial
+        displacements of this Levy flight.
+
+        args
+        ----
+            r           :   1D ndarray, the set of points at which to 
+                            evaluate the CDF. If *None*, defaults to the
+                            same set of bins that are used for spline 
+                            interpolation
+
+        returns
+        -------
+            1D ndarray, dtype float64, size r.shape, the CDF
+
+        """
+        # If not specified, default to the initial real domain support
+        if r is None:
+            r = self.r[:self.cutoff+1]
+
+        # Generate a spline interpolation to the CDF, if not done already
         if not hasattr(self, "_cdf_rad"):
-            self.gen_cdf_rad()
+            self._gen_cdf_rad()
+
+        # Evaluate
         return self._cdf_rad(r)
 
-    def pdf_rad(self, r):
+    def cdf_dev(self, r=None):
         """
-        Approximate the PDF for the 3D radial displacements.
+        Evaluate the first derivative of the cumulative distribution function
+        for the 3D radial displacements of this Levy flight.
 
         args
         ----
-            r       :   1D ndarray, points at which to evaluate PDF
+            r           :   1D ndarray, the set of points at which to evaluate
+                            the CDF's derivative. If *None, defaults to the 
+                            same set of bins that are used for spline interpolation
+                            of the CDF
 
         returns
         -------
-            1D ndarray, the PDF 
-
-        """       
-        if not hasattr(self, "_pdf_rad"):
-            self.gen_pdf_rad()
-        return self._pdf_rad(r)
-
-    def cdf_rad_dev(self, r):
-        """
-        Approximate the first derivative for the CDF for the 3D radial
-        displacements.
-
-        args
-        ----
-            r       :   1D ndarray, evaluation points
-
-        returns
-        -------
-            1D ndarray, the first derivative of the CDF
+            1D ndarray, dtype float64, siez r.shape
 
         """
+        if r is None:
+            r = self.r[:self.cutoff+1]
+
+        # Generate a spline interpolation to this function, if not already done
         if not hasattr(self, "_cdf_rad_dev"):
-            self.gen_cdf_rad_dev()
+            self._gen_cdf_rad()
+
+        # Evaluate
         return self._cdf_rad_dev(r)
 
-    def gen_pdf_rad(self, r=None):
+    def _gen_pdf_rad(self):
         """
-        Generate a spline approximation to the PDF of the radial displacements
-        for the Levy flight.
-
-        args
-        ----
-            r       :   1D ndarray, real space support
-
-        returns
-        -------
-            scipy.interpolate.InterpolatedUnivariateSpline, the spline
-                approximation to the PDF 
+        Generate a spline interpolation to the PDF for the 3D radial 
+        displacements of this Levy flight.
 
         """
-        if r is None:
-            r = np.linspace(*self.interpolation_range)
-        pdf = pdf_from_cf_rad(self.cf, r)
-        pdf = radnorm(r, pdf, d=3)
-        self._pdf_rad = spline(r, pdf/pdf.sum())
-        return self._pdf_rad 
+        # Use a fast formula derived from the 3D Radon transform
+        if self.method == "radon":
+            arg = self.D * self.dt * np.power(np.abs(self.k), self.alpha)
+            cf_dev = (1 - self.alpha * arg) * np.exp(-arg)
+            pdf = np.fft.irfft(cf_dev, n=self.r_full.shape[0])[:self.cutoff+1]
+            pdf /= pdf.sum()
 
-    def gen_cdf_rad(self, r=None):
+        # Similar to the "radon" method, but somewhat more stable/accurate for 
+        # larger values of the dispersion parameter
+        elif self.method == "radon_alt":
+            k_cf = 1.0j * self.k * self.cf(self.k)
+            pdf = -self.r * np.fft.irfft(k_cf, n=self.r_full.shape[0])[:self.cutoff+1]
+            pdf /= pdf.sum()
+
+        # Perform an explicit inverse spherical Fourier transform on 
+        # the radially symmetric PDF
+        elif self.method == "hankel":
+            pdf = pdf_from_cf_rad(self.cf, self.r)
+            pdf = radnorm(self.r, pdf, d=3)
+
+        # Generate a spline interpolation
+        self._pdf_rad = spline(self.r, pdf)
+
+    def _gen_cdf_rad(self):
         """
-        Generate a spline approximation to the CDF of the radial displacements
-        for the Levy flight.
-
-        args
-        ----
-            r       :   1D ndarray, real space support
-
-        returns
-        -------
-            scipy.interpolate.InterpolatedUnivariateSpline, the spline
-                approximation to the CDF 
-
-        """
-        if r is None:
-            r = np.linspace(*self.interpolation_range)
-        pdf = pdf_from_cf_rad(self.cf, r)
-        pdf = radnorm(r, pdf, d=3)
-        cdf = np.cumsum(pdf)
-        self._cdf_rad = spline(r, cdf/cdf[-1])
-        return self._cdf_rad 
-
-    def gen_cdf_rad_dev(self, r=None):
-        """
-        Generate a spline approximation to the first derivative of the CDF for
-        the radial displacements of this Levy flight.
-
-        args
-        ----
-            r       :   1D ndarray, real space support
-
-        returns
-        -------
-            scipy.interpolate.InterpolatedUnivariateSpline
+        Generate a spline interpolation to the CDF for the 3D radial
+        displacements of this Levy flight.
 
         """
-        if not hasattr(self, "_cdf_rad"):
-            self.gen_cdf_rad(r=r)
+        # Evaluate the PDF at the center of each spatial bin
+        pdf = self.pdf(self.rc)
+
+        # If using the "hankel" method, it's possible to erroneously get 
+        # negative values in the PDF, especially near the origin (Gibbs
+        # phenomenon and other effects) and when using insufficient iterations
+        # of the numerical Hankel transform. Even when these are of inconsequential
+        # magnitude, if they are allowed to exist they compromise the monotonicity
+        # of the CDF, which makes inverse CDF sampling impossible. Here, kill
+        # all such instances.
+        # pdf[pdf<0] = 0
+
+        # Accumulate to get the CDF, and include the 0 point
+        cdf = np.concatenate(([0], np.cumsum(pdf)))
+        r = np.concatenate(([0], self.rc))
+
+        # Generate a spline interpolation to the CDF
+        self._cdf_rad = spline(r, cdf)
+
+        # Generate a spline interpolation to the first derivative of the CDF, 
+        # used in inverse CDF sampling
         self._cdf_rad_dev = self._cdf_rad.derivative(1)
-        return self._cdf_rad_dev 
 
     def inverse_cdf(self, p, n_iter=20):
         """
-        Return the inverse CDF of the 3D radial displacements of the Levy
-        flight.
+        Approximate the inverse cumulative distribution function for the 3D
+        radial displacements of this Levy flight.
 
         args
         ----
             p       :   1D ndarray, floats between 0.0 and 1.0
-            n_iter  :   int, the number of iterations of Newton's
-                        algorithm to run
+            n_iter  :   int, the number of iterations of Newton's method to 
+                        use to refine the estimate
 
         returns
         -------
-            float, the inverse CDF at the point
+            1D ndarray, dtype float64, shape p.shape; the inverse CDF
 
         """
-        # Starting point - closest to the sampled value
-        r = np.linspace(*self.interpolation_range)
-
-        # Guarantee monotonicity, necessary for numpy.digitize
-        pdf = self.pdf_rad(r)
+        # Evaluate the CDF on the native support
+        pdf = self.pdf(self.rc)
         pdf[pdf<0] = 0
-        cdf = np.cumsum(pdf)
+        cdf = np.concatenate(([0], np.cumsum(pdf)))
+        r = np.concatenate(([0], self.rc))
 
         # Take the closest bin to each point as the initial guess
         r0 = r[np.digitize(p, cdf)]
 
-        # Do a few rounds of Newton to refine the guesses
-        iter_idx = 0
+        # Do a few rounds of Newton to refine the guess
         for i in range(n_iter):
-            r0 += -0.5 * (self.cdf_rad(r0) - p) / self.cdf_rad_dev(r0)
+            r0 += -0.5 * (self.cdf(r0) - p) / self.cdf_dev(r0)
 
         return r0 
-
-
-
---
+++ strobemodels/simulate/simabel.py
@@ -0,0 +1,180 @@
+#!/usr/bin/env python
+"""
+sim_abel.py -- generate a numerical projection matrix that accomplishes the 
+Abel transform on a predefined support.
+
+The basic idea is this: we have a probability mass function f_{R3} defined on 
+some set of bins R. Given that the PMF defines the 3D radial displacements
+of a radially symmetric random vector R3, we'd like to get the corresponding 
+distribution of radial displacements of R3 when it is projected onto a plane
+that intersects the origin. Call this f_{R2}. 
+
+Then there exists a matrix A such that the PMF of the 2D radial displacements,
+defined on the same support R, is 
+
+    f_{R2} = A . f_{R3}
+
+    (where "." is the matrix dot product)
+
+The purpose of this module is to approximate the matrix *A* using a Monte 
+Carlo method.
+
+Approach. We start with *R*, defined as a random variable such that (1) R lies
+between B0 and B1 and (2) it is the modulus of a 3D random vector with 
+uniform probability density in Euclidean space, provided (1) is satisfied. Then
+the probability density of R goes as 
+
+    f_{R}(r) ~ r^2  if  r in [B0, B1], 0 otherwise
+
+which generates the inverse CDF sampling method
+
+    r = F_{R}^{-1} = ( B0^3 + (B1^3 - B0^3) * p )^{1/3}
+
+such that 
+
+    p ~ Uniform(0, 1)
+
+For each bin [B0, B1], we choose R according to this inverse CDF method and 
+then choose an angular displacement by sampling uniformly on the surface of 
+the corresponding sphere. The result is then projected onto the XY plane,
+and its radius in that plane is accumulated in a new histogram.
+
+This process is repeated for each bin. The result is the matrix *A* described
+above. 
+
+"""
+import os
+import sys 
+import numpy as np 
+import dask 
+from dask.diagnostics import ProgressBar 
+import pandas as pd 
+import matplotlib.pyplot as plt 
+
+# The global bin scheme for this module
+r_edges = np.linspace(0.0, 5.0, 5001)
+r_c = r_edges[:-1] + 0.0005
+
+def radprojsim_bin(B0, B1, n_samples=1000000, n_iter=1, dz=None):
+    """
+    Given a 3D radial displacement R such that R in [B0, B1] and 
+    R is uniformly distributed in the available 3D Euclidean space,
+    return the distribution of 2D radial displacements R2 produced
+    by projecting the vector R onto a plane intersecting the origin.
+
+    args
+    ----
+        B0          :   float, the lower bin limit
+        B1          :   float, the upper bin limit
+        n_samples   :   int, the number of samples to accumulate per
+                        iteration
+        n_iter      :   int, the number of iterations to use 
+        dz          :   float, thickness of the observation slice in z.
+                        If specified, then particles start out with 
+                        a random position in the interval [-dz/2, dz/2]
+                        and are not counted if their final z position lies
+                        outside of this interval. If left *None*, then no
+                        such constraint is imposed. Note that the total 
+                        probability across all bins will generally not 
+                        sum to 1 if *dz* is set.
+
+    returns
+    -------
+        1D ndarray of shape (5000,), the distribution of corresponding
+            2D radial displacements according to the global bin scheme
+
+    """
+    global r_edges 
+
+    result = np.zeros(5000, dtype=np.float64)
+
+    for iter_idx in range(n_iter):
+
+        # Simulate the radial displacements using inverse CDF sampling
+        r = np.cbrt(B0**3 + (B1**3 - B0**3) * np.random.random(size=n_samples))
+
+        # Simulate angular displacements by sampling on the surface of 
+        # the unit sphere
+        a = np.random.normal(size=(n_samples, 3))
+        a = (a.T / np.sqrt((a**2).sum(axis=1))).T 
+
+        # Combine radial and angular parts 
+        a = (a.T * r).T 
+
+        # If desired, simulate a finite range of observation in z
+        if not dz is None:
+            hz = dz * 0.5
+            a[:,0] = a[:,0] + np.random.uniform(-hz, hz, size=n_samples)
+            a = a[np.abs(a[:,0])<=hz, :]
+
+        # Take the XY displacements 
+        r = np.sqrt((a[:,1:]**2).sum(axis=1))
+        H = np.histogram(r, bins=r_edges)[0].astype(np.float64)
+        result += H 
+
+    result /= (n_iter * n_samples)
+    return result 
+
+def radprojsim(n_samples=10000000, n_iter=10, num_workers=8, 
+    out_csv=None, dz=None):
+    """
+    Combine the full numerical approximative Abel transform. For each 
+    bin in the global bin scheme, compute the corresponding distribution
+    of 2D radial displacements.
+
+    args
+    ----
+        n_samples   :   int, the number of samples per iteration per bin
+        n_iter      :   int, the number of iterations per bin
+        num_workers :   int, number of dask workers to use in this job
+        out_csv     :   str, file to save the projection to
+        dz          :   float, axial containment range
+
+    returns
+    -------
+        2D ndarray of shape (5000, 5000), the matrix for performing 
+            a numerical Abel transform
+
+    """
+    global r_edges 
+    result = np.zeros((5000, 5000), dtype=np.float64)
+
+    c = 0
+
+    @dask.delayed 
+    def simulate_bin(i):
+        result = radprojsim_bin(r_edges[i], r_edges[i+1], 
+            n_samples=n_samples, n_iter=n_iter, dz=dz)
+        return result 
+
+    results = [simulate_bin(i) for i in range(5000)]
+
+    if num_workers > 1:
+        scheduler = "processes"
+    else:
+        scheduler = "single-threaded"
+
+    with ProgressBar():
+        results = dask.compute(*results, scheduler=scheduler, num_workers=num_workers)
+
+    # Accumulate into a single ndarray
+    A = np.zeros((5000, 5000), dtype=np.float64)
+    for i, r in enumerate(results):
+        A[:,i] = r 
+
+    # Format as a dataframe and save 
+    if not out_csv is None:
+        cols = ["%.3f" % i for i in r_edges[1:]]
+        df = pd.DataFrame(A, columns=cols)
+        df["r_right_edge_um"] = r_edges[1:]
+        df.to_csv(out_csv, index=False)
+
+    return A
+
+if __name__ == '__main__':
+    #result = radprojsim(n_samples=1000000, n_iter=10, num_workers=1)
+    result = radprojsim(n_samples=1000000, n_iter=10, num_workers=1, dz=0.7,
+        out_csv="abel_transform_dz-0.7um.csv")
+    plt.imshow(result, vmax=result.max()*0.05, cmap='gray')
+    plt.show(); plt.close()
+
